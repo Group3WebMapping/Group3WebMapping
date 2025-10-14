6c89fa6f962f791a3b12e89fc78406e21b5e9eb0
mseaborn2020 <mseaborn2020@gmail.com>
2025-10-14 17:42:40 -0500

    edit

--- map.js
+++ map.js
@@ -6,21 +6,26 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
-
-function PointDistance () {
-  var PointA = turf.point([-97.941, 29.885]);
-  var PointB = turf.point([-97.939, 29.867]);
+    function DestinationPoint() {
+  var start = turf.point([-97.9384, 29.8884]);
+  var distance = 1;
+  var bearing = 45;
   var options = { units: "miles" };
-  var distance = turf.distance(PointA, PointB, options);
+  var destination = turf.destination(start, distance, bearing, options);
+  var destCoords = destination.geometry.coordinates;
+  L.marker([29.8884, -97.9384])
+    .addTo(map)
+    .bindPopup("Start Point (San Marcos)");
 
-  L.marker([29.885,-97.941]) 
-  .addTo(map).bindPopup("Distance: " + distance + " miles");
-  L.marker([29.867,-97.939])
-  .addTo(map).bindPopup("Distance: " + distance + " miles");
+  L.marker([destCoords[1], destCoords[0]])
+    .addTo(map)
+    .bindPopup("Destination<br>Distance: " + distance + " mile(s)<br>Bearing: " + bearing + "°");
 
-  L.polyline([[29.885,-97.941],[29.867, -97.939]])
+  L.polyline([
+    [29.8884, -97.9384],
+    [destCoords[1], destCoords[0]]
+  ])
   .addTo(map)
-  .bindPopup("Distance: " + distance + " miles");
+  .bindPopup("Route: " + distance + " mile(s)");
 }
-
-PointDistance();
+DestinationPoint();
