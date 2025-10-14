330da4a95eb111633cebecdc351f56924de3059c
mseaborn2020 <mseaborn2020@gmail.com>
2025-10-14 15:37:30 -0500

    fix

--- map.js
+++ map.js
@@ -6,16 +6,3 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
-    var polygonCoords = [
-    [30.2800, -97.7500],
-    [30.2800, -97.7000],
-    [30.2400, -97.7000],
-    [30.2400, -97.7500]
-  var polygon = L.polygon(polygonCoords, { color: 'blue' }).addTo(map);
-  polygon.bindPopup('Click to calculate area.');
-  polygon.on('click', function () {
-      var geojson = polygon.toGeoJSON();
-      var area = turf.area(geojson);
-      polygon.setPopupContent('Area: ' + area.toFixed(2) + ' square meters');
-      polygon.openPopup();
-  });
