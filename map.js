8746ecda73e6144702e4174ab02be940fcb09eeb
mseaborn2020 <mseaborn2020@gmail.com>
2025-10-14 15:38:06 -0500

    function edit

--- map.js
+++ map.js
@@ -6,3 +6,30 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
+    var polygonCoords = [
+        [30.2800, -97.7500],  // Top-left corner
+        [30.2800, -97.7000],  // Top-right corner
+        [30.2400, -97.7000],  // Bottom-right corner
+        [30.2400, -97.7500]   // Bottom-left corner
+    ];
+
+    // Create the polygon and add it to the map
+    var polygon = L.polygon(polygonCoords, { color: 'blue' }).addTo(map);
+
+    // Bind a basic popup to the polygon before the click event
+    polygon.bindPopup('Click to calculate area.');
+
+    // Add an event listener for the click event
+    polygon.on('click', function () {
+        // Convert the Leaflet polygon to GeoJSON
+        var geojson = polygon.toGeoJSON();
+
+        // Calculate the area using Turf.js (in square meters)
+        var area = turf.area(geojson);
+
+        // Update the popup content with the calculated area
+        polygon.setPopupContent('Area: ' + area.toFixed(2) + ' square meters');
+
+        // Open the popup after setting the content
+        polygon.openPopup();
+    });
