1190a39d9f9ab4656d1246f5b1c10e434d3d3828
pef36 <pef36@txstate.edu>
2025-10-15 10:29:55 -0500

    polygon function

--- map.js
+++ map.js
@@ -6,3 +6,21 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
+      var polygon = turf.polygon([
+        [[-97.999144, 29.753413],
+        [-97.710054, 30.023649],
+        [-97.716932, 30.029089],
+        [-97.859382, 30.132646],
+        [-98.172415, 30.355621],
+        [-98.298284, 30.038085],
+        [-98.030800, 29.847320],
+        [-97.999144, 29.753413]]
+      ])
+      L.geoJSON(polygon,{
+    style: {
+        color: "blue",
+        weight: 3,
+        fillColor: "light blue",
+        fillOpacity: 0.35,
+        }
+      }).addTo(map);
